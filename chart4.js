41750494e2aabee24970bc3877ff04d9db829853
MACAW\Mirella <Mirella.Kersten@macaw.nl>
2017-04-10 15:18:21 +0200

    Chart is working, CSS is not SCSS!

--- chart4.js
+++ chart4.js
@@ -1,3 +1,5 @@
+//Tutorial from https://bost.ocks.org/mike/bar/3/
+
 /*  Rotating a bar chart into a column chart largely involves swapping x with y. 
     However, a number of smaller incidental changes are also required. 
     This is the cost of working directly with SVG rather than using a high-level visualization grammar like ggplot2. 
@@ -24,35 +26,35 @@ var g4 = chart4.append("g")
     .attr("transform", "translate(" + margin4.left + "," + margin4.top + ")");
 
 d3.tsv("data_chart4.tsv", function(d) {
-  d.frequency = +d.frequency;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+    d.frequency = +d.frequency;
+    return d;
+    }, function(error, data) {
+    if (error) throw error;
 
-  x4.domain(data.map(function(d) { return d.letter; }));
-  y4.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+    x4.domain(data.map(function(d) { return d.letter; }));
+    y4.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-  g4.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height4 + ")")
-      .call(d3.axisBottom(x4));
+    g4.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height4 + ")")
+            .call(d3.axisBottom(x4));
 
-  g4.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y4).ticks(10, "%"))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
+    g4.append("g")
+            .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y4).ticks(10, "%"))
+        .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Frequency");
 
-  g4.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("width", x4.bandwidth())
-      .attr("height", function(d) { return height4 - y(d.frequency); });
+    g4.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x4(d.letter); })
+            .attr("y", function(d) { return y4(d.frequency); })
+            .attr("width", x4.bandwidth())
+            .attr("height", function(d) { return height4 - y4(d.frequency); });
 });
